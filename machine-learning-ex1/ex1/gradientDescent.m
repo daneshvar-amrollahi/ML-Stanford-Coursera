ec0296e41a4d711655fca4a56a86910e0b13e7de
daneshvar.amrollahi <amrollahi.daneshvar@gmail.com>
2020-09-25 13:15:21 +0330

    Gradient Descent added

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -7,6 +7,8 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+delta = zeros(2, 1);
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,11 +18,21 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
+    sum = 0;
+    for i = 1:m
+        sum = sum + ((theta(1) * X(i, 1) + theta(2) * X(i, 2)) - y(i)) * X(i, 1);     
+    endfor
+    
+    delta(1) = (1 / m) * sum;
+    
+    sum = 0;
+    for i = 1:m
+        sum = sum + ((theta(1) * X(i, 1) + theta(2) * X(i, 2)) - y(i)) * X(i, 2);
+    endfor
+    
+    delta(2) = (1 / m) * sum;
+    
+    theta = theta - alpha * delta;
 
 
     % ============================================================
