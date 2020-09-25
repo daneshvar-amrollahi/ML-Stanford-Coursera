a987afa5ec82b51c6330b66fdcf8a45441f5573e
daneshvar.amrollahi <amrollahi.daneshvar@gmail.com>
2020-09-25 12:37:33 +0330

    Cost function added

--- machine-learning-ex1/ex1/ex1.m
+++ machine-learning-ex1/ex1/ex1.m
@@ -45,7 +45,6 @@ m = length(y); % number of training examples
 % Plot Data
 % Note: You have to complete the code in plotData.m
 plotData(X, y);
-
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
