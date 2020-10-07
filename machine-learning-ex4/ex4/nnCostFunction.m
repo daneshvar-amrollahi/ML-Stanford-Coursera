a689a7ef070df7703a825da7152ace25db8addd6
daneshvar.amrollahi <amrollahi.daneshvar@gmail.com>
2020-10-07 22:15:58 +0330

    BackPropagation Implemented

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -39,43 +39,7 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 
-X = [ones(m, 1) X];
-for i = 1: m,
-  a1 = X(i,:);
-  
-  z2 = Theta1 * (a1)'; %25 x 401 * 401 x 1 --> 25 x 1
-  a2 = sigmoid(z2);
-  
-  
-  a2 = [1; a2]; %25 x 1 --> 26 x 1
-  z3 = Theta2 * a2; %10 x 26 -* 26 x 1 --> 10 x 1
-  a3 = sigmoid(z3); %10 x 1
-  
-  curY = zeros(num_labels, 1);
-  curY(y(i)) = 1; 
-  J += sum( -curY .* log(a3) - ((1 - curY) .* log(1 - a3) )  ); %instead of looping from 1 to k
-  
-endfor
 
-J /= m;
-
-reg = 0;
-for j = 1: hidden_layer_size,
-  for k = 2: input_layer_size + 1,
-    reg += (Theta1(j, k) ^ 2);
-  endfor
-endfor
-
-for j = 1: num_labels,
-  for k = 2: hidden_layer_size + 1,
-    reg += (Theta2(j, k) ^ 2);
-  endfor
-endfor
-
-J += (lambda / (2 * m)) * reg;
-
-
-%
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -91,31 +55,37 @@ J += (lambda / (2 * m)) * reg;
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
-% Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
-%
 
+for i=1:m
+    
+    a1 = X(i,:); %1 x 400
+    a1 = a1';%400 x 1
+    a1 = [1; a1];%401 x 1
+    
+    z2 = Theta1 * a1;
+    a2 = [1; sigmoid(z2)];
+    z3 = Theta2 * a2;
+    a3 = sigmoid(z3);
 
+    curY = (1:num_labels)' == y(i);
+    J = J + sum(-curY .* log(a3) - (1 - curY) .* log(1 - a3));
 
+    % backprop
+    delta3 = a3 - curY;
+    delta2 = Theta2' * delta3 .* (a2 .* (1 - a2));
+    Theta2_grad = Theta2_grad + delta3 * a2';
+    Theta1_grad = Theta1_grad + delta2(2:end) * a1';
+end;
 
+% scaling cost function and gradients
+J = J / m;
+Theta1_grad = Theta1_grad / m;
+Theta2_grad = Theta2_grad / m;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+% regularization
+J = J + (lambda / (2 * m)) * (sumsq(Theta1(:, 2:end)(:)) + sumsq(Theta2(:, 2:end)(:)));
+Theta1_grad = Theta1_grad + (lambda / m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = Theta2_grad + (lambda / m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 
 % -------------------------------------------------------------
 
@@ -123,6 +93,5 @@ J += (lambda / (2 * m)) * reg;
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
-
 end
+
