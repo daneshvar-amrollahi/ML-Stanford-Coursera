2c05db316a417ba50be8a5855fadfb47c2e828d0
daneshvar.amrollahi <amrollahi.daneshvar@gmail.com>
2020-10-29 00:22:02 +0330

    SVM Parameters Choosing Implemented

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,7 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+minErr = 1;
+for i = 1:8,
+  for j = 1:8,
+    curC = values(i);
+    curSigma = values(j);
+    
+    model = svmTrain(X, y, curC, @(x1, x2) gaussianKernel(x1, x2, curSigma));
+    predictions = svmPredict(model, Xval);
+    
+    curErr = mean(double(predictions ~= yval));
+    if (curErr < minErr),
+      C = curC;
+      sigma = curSigma;
+      minErr = curErr;
+    endif
+  endfor
+endfor
 
 
 
